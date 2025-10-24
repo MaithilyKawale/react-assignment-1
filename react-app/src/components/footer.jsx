23bbfe14d8241b5147b37901d6f767fef61098f2
MaithilyKawale <maithilyworkspace@gmail.com>
2025-10-24 11:33:55 +0530

    footer component added

---
+++ react-app/src/components/footer.jsx
@@ -0,0 +1,6 @@
+
+function Footer({ year }) {
+    return <footer>© {year} My React App. All rights reserved.</footer>
+}
+
+export default Footer;
--- react-app/src/main.jsx
+++ react-app/src/main.jsx
@@ -2,11 +2,13 @@ import { createRoot } from 'react-dom/client'
 import Header from './components/Header.jsx'
 import Card from './components/card.jsx'
 import image1 from './assests/image1.jpg';
+import Footer from './components/footer.jsx';
 createRoot(document.getElementById('root')).render(
   <>
  <Header title='Welcome to my app'/>
  <div>
  <Card name="Maithily" description="React developer" imageUrl={image1}/>
  </div>
+ <Footer year={2024}/>
 </>
 )
