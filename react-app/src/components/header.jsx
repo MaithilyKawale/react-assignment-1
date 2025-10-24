02873ad19c430c9bfb74ae687d0e8cae52fc89c1
MaithilyKawale <maithilyworkspace@gmail.com>
2025-10-24 12:20:48 +0530

    image added

--- react-app/src/components/header.jsx
+++ react-app/src/components/header.jsx
@@ -1,6 +1,7 @@
 function Header({ title }) {
     const headerstyle = {
-        color: 'blue',
+        color: 'black',
+        backgroundColor: '#f1f1f1',
         textAlign: 'center',
         padding: '10px'
     }
--- react-app/src/main.jsx
+++ react-app/src/main.jsx
@@ -2,12 +2,14 @@ import { createRoot } from 'react-dom/client'
 import Header from './components/Header.jsx'
 import Card from './components/card.jsx'
 import image1 from './assests/image1.jpg';
+import image2 from './assests/image2.jpg';
 import Footer from './components/footer.jsx';
 createRoot(document.getElementById('root')).render(
   <>
  <Header title='Welcome to my app'/>
  <div>
  <Card name="Maithily" description="React developer" imageUrl={image1}/>
+ <Card name="Sunita" description="Frontend developer" imageUrl={image2}/>
  </div>
  <Footer year={2024}/>
 </>
